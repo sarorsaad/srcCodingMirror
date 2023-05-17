9edb501288840d39cd30fb6b58ebc077cbe06123
elsaad92 <99730108+elsaad92@users.noreply.github.com>
2023-05-17 17:49:46 +0300

    pgAdmin4 postgres database configration

--- project/settings.py
+++ project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Boards_django',
+        'USER': 'postgres',
+        'PASSWORD': '1290@asd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
